4b2864c298e023756c9d5ab952f5d3a81c4cd4a6
pokio <32353873+pokiiio@users.noreply.github.com>
2018-09-02 23:31:54 +0900

    Use jma data.

--- create_weather_image.py
+++ create_weather_image.py
@@ -9,35 +9,45 @@ from StringIO import StringIO
 
 WIDTH = 264
 HEIGHT = 176
-URL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=140010"
+URL = "https://www.jma.go.jp/jp/yoho/320.html"
 FONT_SIZE = 18
 FONT_PATH = path.dirname(path.abspath(__file__)) + "/mplus-2p-heavy.ttf"
 FONT = ImageFont.truetype(FONT_PATH, FONT_SIZE, encoding="unic")
 IMAGE_FILE = path.dirname(path.abspath(__file__)) + "/image.png"
 
-jsonData = requests.get(URL).json()
+htmlData = requests.get(URL).text
+forecastData = []
+forecastData.append(htmlData.split("<th class=\"weather\">")[1])
+forecastData.append(htmlData.split("<th class=\"weather\">")[2])
 
 image = Image.new("RGB", (WIDTH, HEIGHT), (255, 255, 255))
 draw = ImageDraw.Draw(image)
 draw.font = FONT
 
 for index in range(2):
-    forecast = jsonData["forecasts"][index]
-    icon = Image.open(StringIO(requests.get(forecast["image"]["url"]).content))
+    forecast = forecastData[index]
+    iconUrl = forecast.split("<img src=\"")[1].split("\"")[0]
+    iconUrl = "https://www.jma.go.jp/jp/yoho/" + iconUrl
+    icon = Image.open(StringIO(requests.get(iconUrl).content)).convert("RGBA")
+    background = Image.new('RGBA', icon.size, 'white')
+    icon = Image.alpha_composite(background, icon)
+
     icon = icon.resize((100, 62), Image.LANCZOS)
     image.paste(icon, (10, 13 + index * 88))
 
     minTemp = u"―℃"
     maxTemp = u"―℃"
 
-    if forecast["temperature"]["min"] is not None:
-        minTemp = forecast["temperature"]["min"]["celsius"] + u"℃"
+    if forecast.find("<td class=\"min\">") > -1:
+        minTemp = forecast.split("<td class=\"min\">")[
+            1].split("</td>")[0].replace(u"度", "") + u"℃"
 
-    if forecast["temperature"]["max"] is not None:
-        maxTemp = forecast["temperature"]["max"]["celsius"] + u"℃"
+    if forecast.find("<td class=\"max\">") > -1:
+        maxTemp = forecast.split("<td class=\"max\">")[
+            1].split("</td>")[0].replace(u"度", "") + u"℃"
 
     draw.text((110, 13 + index * 88),
-              forecast["dateLabel"] + " : " + forecast["telop"], (0, 0, 0))
+              forecast.split("<br>")[0].replace("\n", "") + " : " + forecast.split("title=\"")[1].split("\"")[0], (0, 0, 0))
     draw.text((110, 13 + 18 + index * 88), u"最高気温 " + maxTemp, (0, 0, 0))
     draw.text((110, 13 + 36 + index * 88), u"最低気温 " + minTemp, (0, 0, 0))
 
